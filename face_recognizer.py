5bb59507791b5e5b530b63fa831affbca33fc46c
lucselmes <lselmes@gmail.com>
2024-02-11 17:32:44 -0500

    Add caching mechanism for saving and loading faces

--- face_recognizer.py
+++ face_recognizer.py
@@ -1,15 +1,15 @@
 import face_recognition
 import pickle as pkl
+import os
 
 class FaceStorage():
     """ Handles storage and retrieval of known faces """
-    def __init__(self, init_from_cache):
-        self.cache_path = "" # Need to implement and test caching logic
+    def __init__(self, init_from_cache, cache_path):
+        self.cache_path = cache_path
+        self.known_face_encodings = []
+        self.known_face_names = []
         if init_from_cache:
-            self.read_faces_from_file()
-        else:
-            self.known_face_encodings = []
-            self.known_face_names = []
+            self.load_cache()
 
     def add_face_to_dataset(self, name, face_encoding):
         self.known_face_encodings.append(face_encoding)
@@ -20,30 +20,27 @@ class FaceStorage():
         self.known_face_encodings.pop(index)
         self.known_face_names.pop(index)
 
-    def backup_faces_to_file(self):
+    def save_cache(self):
         """ Saves the known faces to a pickle file """
         with open(self.cache_path, 'wb') as f:
             pkl.dump(self.known_face_encodings, f)
             pkl.dump(self.known_face_names, f)
 
-    def read_faces_from_file(self):
+    def load_cache(self):
         """ Reads the known faces from a pickle file """
-        # If the attributes are not empty, then ask for confirmation of overwriting
-        if self.known_face_encodings or self.known_face_names:
-            print("Reading faces from file will overwrite current face encoding. Do you wish to continue? (y/n)")
-            response = input()
-            if response.lower() != 'y':
-                return
-
-        with open(self.cache_path, 'rb') as f:
-            self.known_face_encodings = pkl.load(f)
-            self.known_face_names = pkl.load(f)
+        print("Loading cache from file")
+        if os.path.exists(self.cache_path):
+            with open(self.cache_path, 'rb') as f:
+                self.known_face_encodings = pkl.load(f)
+                self.known_face_names = pkl.load(f)
+        else:
+            print("Cache file not found. Starting with an empty database.")
 
 class FaceRecognizer(FaceStorage):
     """ Handles tasks related to recognizing faces """
 
-    def __init__(self, init_from_cache):
-        super(FaceRecognizer, self).__init__(init_from_cache=init_from_cache)
+    def __init__(self, init_from_cache, cache_path):
+        super(FaceRecognizer, self).__init__(init_from_cache=init_from_cache, cache_path=cache_path)
 
     def detect_faces(self, frame):
         # Find all the faces and face encodings in the current frame of video
--- main.py
+++ main.py
@@ -4,23 +4,22 @@ import argparse
 import cv2
 
 class RecognizerStream():
-    def __init__(self, camera_index, resize_factor, init_from_cache):
-        self.gui_handler = GUIHandler(camera_index, resize_factor)
-        self.face_recognizer = FaceRecognizer(init_from_cache)
+    def __init__(self, camera_index, resize_factor, init_from_cache, cache_path):
+        self.gui_handler = GUIHandler(camera_index=camera_index, 
+                                      resize_factor=resize_factor)
+        self.face_recognizer = FaceRecognizer(init_from_cache=init_from_cache, 
+                                              cache_path=cache_path)
         self.processing_unknown_face = False # Flag to avoid a prompt for another unknown face when we are already processing one
 
     def handle_and_add_unknown_face(self, frame, face_location, face_encoding):
-
         # Display prompt to add the unknown face to the database
         name = self.gui_handler.handle_unknown_face(frame, face_location)
-
         # Add the unknown face to the database
         if name:
             print(f"Adding {name} to the database")
             self.face_recognizer.add_face_to_dataset(name, face_encoding)
         else:
             print("Unknown face not added to the database")
-
         self.processing_unknown_face = False
 
     def run_processing_logic(self, frame):
@@ -38,6 +37,14 @@ class RecognizerStream():
                 self.handle_and_add_unknown_face(frame, face_locations[i], face_encodings[i])
 
         return face_locations, face_names
+    
+    def handle_key_press(self, key):
+        if key == ord('q'):
+            return True
+        elif key == ord('s'):
+            self.face_recognizer.save_cache()
+            print("Current cache of faces saved to file")
+        return False
 
     def stream_video(self):
         process_this_frame = True # process_this_frame will be used to skip everything other frame -> Less smooth but faster
@@ -53,8 +60,9 @@ class RecognizerStream():
             # Display the results
             self.gui_handler.display_frame_with_faces(frame, face_locations, face_names)
 
-            # Hit 'q' on the keyboard to quit!
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            key = cv2.waitKey(1) & 0xFF
+
+            if self.handle_key_press(key):
                 break
 
         # Release handle to the webcam
@@ -66,9 +74,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Face recognition using webcam')
     parser.add_argument('--camera-index', type=int, default=1, dest="camera_index", help='Index of the camera to use. Defaults to 1 (external camera). 0 is the built-in camera.')
     parser.add_argument('--resize-factor', type=float, default=0.25, dest="resize_factor", help='Amount to resize the video frames when processing')
-    parser.add_argument('--init-from-cache', action='store_true', dest="init_from_cache", help='Initialize the face recognizer from the cache')
+    parser.add_argument('--init-from-cache', action='store_false', dest="init_from_cache", help='Initialize the face recognizer from the cache')
+    parser.add_argument('--cache-path', type=str, default="faces.cache", dest="cache_path", help='Path to the cache file')
     args = parser.parse_args()
-    stream = RecognizerStream(args.camera_index, args.resize_factor, args.init_from_cache)
+    stream = RecognizerStream(camera_index=args.camera_index, 
+                              resize_factor=args.resize_factor, 
+                              init_from_cache=args.init_from_cache, 
+                              cache_path=args.cache_path)
 
     # Check if the camera is working before starting the stream
     if not stream.gui_handler.is_camera_working():
